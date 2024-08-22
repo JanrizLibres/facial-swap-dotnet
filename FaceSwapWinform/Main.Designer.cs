a85b62924b48ff58db9a99bcf5b8323f8011b2a5
Janriz Libres <janriz.libres@elinnovtech.com>
2024-08-22 19:13:24 +0800

    initial commit

---
+++ FaceSwapWinform/Main.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace FaceSwapWinform
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            printDocument1 = new System.Drawing.Printing.PrintDocument();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Location = new Point(10, 9);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(679, 320);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 338);
+            Controls.Add(pictureBox1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Facial Swap Prototype";
+            Load += Main_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ FaceSwapWinform/Main.cs
@@ -0,0 +1,101 @@
+using DlibDotNet;
+using DlibDotNet.Extensions;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+using Size = System.Drawing.Size;
+
+namespace FaceSwapWinform
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(1280, 720);
+            this.CenterToScreen();
+
+            // load the input image
+            var bitmap = Bitmap.FromFile("./input.jpg") as Bitmap;
+
+            // load the face to swap
+            var bitmapToSwap = Bitmap.FromFile("./selfie.jpg") as Bitmap;
+
+            // process image
+            var newBitmap = ProcessImage(bitmap!, bitmapToSwap!);
+
+            // show new image
+            pictureBox1.Image = newBitmap;
+        }
+
+        private static Bitmap ProcessImage(Bitmap image, Bitmap newImage)
+        {
+            // set up Dlib facedetectors and 
+            using var fd = Dlib.GetFrontalFaceDetector();
+            using var sp = ShapePredictor.Deserialize("./shape_predictor_68_face_landmarks.dat");
+
+            // convert image to dlib format
+            //var compatibleImage = image.Clone(new Rectangle(0, 0, image.Width, image.Height), PixelFormat.Format24bppRgb);
+            var img = image.ToArray2D<RgbPixel>();
+
+            // find target's faces in image
+            var faces = fd.Operator(img);
+            var target = faces[1];
+
+            // get target's landmark points
+            var targetShape = sp.Detect(img, target);
+            var targetPoints = (from i in Enumerable.Range(0, (int)targetShape.Parts)
+                                 let p = targetShape.GetPart((uint)i)
+                                 select new OpenCvSharp.Point(p.X, p.Y)).ToArray();
+
+            // get convex hull of target's points
+            var hull = Cv2.ConvexHullIndices(targetPoints);
+            var targetHull = from i in hull
+                              select targetPoints[i];
+
+            // find landmark points in face to 
+            var imgMark = newImage.ToArray2D<RgbPixel>();
+            var faces2 = fd.Operator(imgMark);
+            var source = faces2[0];
+            var sourceShape = sp.Detect(imgMark, source);
+            var sourcePoints = (from i in Enumerable.Range(0, (int)sourceShape.Parts)
+                              let p = sourceShape.GetPart((uint)i)
+                              select new OpenCvSharp.Point(p.X, p.Y)).ToArray();
+
+            // get convex hull of source's points
+            var hull2 = Cv2.ConvexHullIndices(targetPoints);
+            var sourceHull = from i in hull2
+                           select sourcePoints[i];
+
+            // calculate Delaunay triangles
+            var triangles = Utility.GetDelaunayTriangles(targetHull);
+
+            // get transformations to warp the new face onto target's face
+            var warps = Utility.GetWarps(sourceHull, targetHull, triangles);
+
+            // apply the warps to the new face to prep it for insertion into the main image
+            var warpedImg = Utility.ApplyWarps(newImage, image.Width, image.Height, warps);
+
+            // prepare a mask for the warped image
+            var mask = new Mat(image.Height, image.Width, MatType.CV_8UC3);
+            mask.SetTo(0);
+            Cv2.FillConvexPoly(mask, targetHull, new Scalar(255, 255, 255), LineTypes.Link8);
+
+            // find the center of the warped face
+            var r = Cv2.BoundingRect(targetHull);
+            var center = new OpenCvSharp.Point(r.Left + r.Width / 2, r.Top + r.Height / 2);
+
+            // blend the warped face into the main image
+            var selfie = BitmapConverter.ToMat(image);
+            var blend = new Mat(selfie.Size(), selfie.Type());
+            Cv2.SeamlessClone(warpedImg, selfie, mask, center, blend, SeamlessCloneMethods.NormalClone);
+
+            // return the modified main image
+            return BitmapConverter.ToBitmap(blend);
+        }
+    }
+}
